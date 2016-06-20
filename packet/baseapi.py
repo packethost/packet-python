59ae57c38f0ee4bb58cf4c46855827a69d0e3093
Niels Grewe <niels.grewe@halbordnung.de>
2016-06-21 00:32:15 +0200

    Improve error handling
    * Add a 'cause' property to packet.baseapi.Error for a python 2
      compatible way of exception chaining
    * Catch errors generated by requests and wrap them for a unified
      exception handling interface in consumers.

--- packet/baseapi.py
+++ packet/baseapi.py
@@ -6,7 +6,14 @@ import requests
 
 class Error(Exception):
     """Base exception class for this module"""
-    pass
+    def __init__(self, msg, cause=None):
+        super(Error, self).__init__(msg)
+        self._cause = cause
+
+    @property
+    def cause(self):
+        """The underlying exception causing the error, if any."""
+        return self._cause
 
 
 class JSONReadError(Error):
@@ -38,20 +45,28 @@ class BaseAPI(object):
         headers_str = str(headers).replace(self.auth_token.strip(), 'TOKEN')
         self._log.debug('%s %s %s %s' %
                         (type, url, params, headers_str))
-
-        if type == 'GET':
-            resp = requests.get(url, headers=headers)
-        elif type == 'POST':
-            resp = requests.post(url, headers=headers, data=json.dumps(params))
-        elif type == 'DELETE':
-            resp = requests.delete(url, headers=headers)
-        elif type == 'PATCH':
-            resp = requests.patch(url, headers=headers, data=json.dumps(params))
-        else:
-            raise Error(
-                'method type not recognizes as one of GET, POST, DELETE or PATCH: %s' % type
-            )
-
+        try:
+            if type == 'GET':
+                resp = requests.get(url, headers=headers)
+            elif type == 'POST':
+                resp = requests.post(url, headers=headers,
+                                     data=json.dumps(params))
+            elif type == 'DELETE':
+                resp = requests.delete(url, headers=headers)
+            elif type == 'PATCH':
+                resp = requests.patch(url, headers=headers,
+                                      data=json.dumps(params))
+            else:
+                raise Error(
+                    'method type not recognizes as one of GET, POST, DELETE or PATCH: %s' % type
+                )
+        except requests.exceptions.RequestException as e:
+            raise Error('Communcations error: %s' % str(e), e)
+        try:
+            resp.raise_for_status()
+        except requests.HTTPError as e:
+            raise Error('Error {0}: {1}'.format(resp.status_code,
+                                                resp.reason), e)
         if not resp.content:
             data = None
         elif resp.headers['content-type'].startswith("application/json"):
@@ -59,7 +74,7 @@ class BaseAPI(object):
                 data = resp.json()
             except ValueError as e:
                 raise JSONReadError(
-                    'Read failed: %s' % e.message
+                    'Read failed: %s' % e.message, e
                 )
         else:
             data = resp.content
