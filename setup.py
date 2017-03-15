20b80bd8575cc25971eced3f3770884f78cd9b96
Aaron Welch <welch@packet.net>
2017-03-15 10:01:13 -0400

    new pypi release

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if os.path.isfile("README.md"):
 
 setup(
     name='packet-python',
-    version='1.32',
+    version='1.33',
     description='Packet API client',
     author='Aaron Welch ( http://www.packet.net )',
     author_email='welch@packet.net',
