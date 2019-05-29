ebef138938d519ff2d4321d4f47256f40e5cef4e
Manuel Mendez <mmendez534@gmail.com>
2019-05-29 17:33:29 -0400

    add nix/direnv files

---
+++ shell.nix
@@ -0,0 +1,26 @@
+let
+  _pkgs = import <nixpkgs> {};
+in
+{ pkgs ? import (_pkgs.fetchFromGitHub { owner = "NixOS";
+                                         repo = "nixpkgs-channels";
+                                         # nixos-unstable @2019-05-28
+                                         rev = "eccb90a2d997d65dc514253b441e515d8e0241c3";
+                                         sha256 = "0ffa84mp1fgmnqx2vn43q9pypm3ip9y67dkhigsj598d8k1chzzw";
+                                       }) {}
+}:
+
+with pkgs;
+
+mkShell {
+  buildInputs = [
+    python3
+    python3Packages.black
+    python3Packages.flake8
+    python3Packages.pylama
+    python3Packages.pytest
+    python3Packages.pytestcov
+    python3Packages.requests
+    python3Packages.requests-mock
+    python3Packages.tox
+  ];
+}
