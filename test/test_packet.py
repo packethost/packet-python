552b97331805d0ef98fa9da5972530e800482169
hwnjng <harry@packet.com>
2019-07-29 17:13:30 -0400

    fmt and lint

--- test/test_packet.py
+++ test/test_packet.py
@@ -71,7 +71,7 @@ class PacketManagerTest(unittest.TestCase):
             str(device)
             repr(device)
             self.assertIsInstance(device, packet.Device)
-    
+
     # TODO figure out how to properly handle this test case
     # def test_list_all_devices(self):
     #     devices = self.manager.list_all_devices("438659f0")
