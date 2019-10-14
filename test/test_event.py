466b7ae21a46b2f3a70896750cb16115f4805345
Manuel Mendez <mmendez534@gmail.com>
2019-10-14 14:12:40 -0400

    Add SPDX header to test files

--- test/test_event.py
+++ test/test_event.py
@@ -1,3 +1,5 @@
+# SPDX-License-Identifier: LGPL-3.0-only
+
 import os
 import sys
 import unittest
--- test/test_vpn.py
+++ test/test_vpn.py
@@ -1,3 +1,5 @@
+# SPDX-License-Identifier: LGPL-3.0-only
+
 from __future__ import print_function
 
 import os
