6db5f1c83c951efcb085a7ce5b0780de07ff2473
Manuel Mendez <mmendez534@gmail.com>
2019-10-14 14:14:18 -0400

    Add source file encoding header to tests.

--- test/test_event.py
+++ test/test_event.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # SPDX-License-Identifier: LGPL-3.0-only
 
 import os
--- test/test_vpn.py
+++ test/test_vpn.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # SPDX-License-Identifier: LGPL-3.0-only
 
 from __future__ import print_function
